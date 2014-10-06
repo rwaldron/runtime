52dbc66b531a7728993604bd19fa1c3e3cea2713
Rick Waldron <waldron.rick@gmail.com>
2014-10-06 14:32:11 -0400

    Adds test with built in

--- test/issues/issue-runtime-487.js
+++ test/issues/issue-runtime-487.js
@@ -1,6 +1,6 @@
 var tap = require('../tap');
 
-tap.count(2);
+tap.count(4);
 
 function Test() {
   this.alpha = "hi";
@@ -18,3 +18,23 @@ for (var prop in test) {
 }
 
 tap.eq(props.length, 2);
+
+
+test = new Array();
+props = [];
+
+for (var prop in test) {
+  props.push(prop);
+}
+
+tap.eq(props.length, 0);
+
+test.push(1);
+
+for (var prop in test) {
+  props.push(prop);
+}
+
+tap.eq(props.length, 1);
+tap.eq(props[0], 1);
+
